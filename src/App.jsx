1961ef914043bb0eb2940e7d090ced09530b84ff
Jessica Trindade Nascimento de Brito <141635580+jessicatrindadeads@users.noreply.github.com>
2026-08-19 11:55:33 -0300

    refactor: aprimora src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import './globalStyle.scss'
@@ -7,21 +7,17 @@ import Voluntario from './pages/voluntario/Voluntario'
 import ComoAjudar from './pages/como-ajudar/ComoAjudar'
 
 function App() {
-
-
   return (
-    <>
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/voluntario' element={<Voluntario/>}/>
-        <Route path="/como-ajudar" element={<ComoAjudar/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/voluntario" element={<Voluntario />} />
+        <Route path="/como-ajudar" element={<ComoAjudar />} />
+        <Route path="*" element={<Home />} />
       </Routes>
       <Footer />
-      
     </BrowserRouter>
-    </>
   )
 }
 

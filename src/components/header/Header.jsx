3dec7993f5ba8400c4d0928bbeb8e60320010a61
Jessica Trindade Nascimento de Brito <141635580+jessicatrindadeads@users.noreply.github.com>
2026-08-19 11:55:46 -0300

    refactor: aprimora src/components/header/Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,40 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import s from './header.module.scss'
 import Logo  from '../../assets/logo.png'
 import Menu from '../../assets/menu.png'
 
 const Header = () => {
+  const [menuAberto, setMenuAberto] = useState(false)
+
+  const fecharMenu = () => setMenuAberto(false)
+
   return (
     <header className={s.header}>
-      <div className={s.BoxLogo}>
+      <div className={s.boxLogo}>
         <img src={Logo} alt="Imagem de logo do site Médicos Voluntários" />
-        <Link to='/'>Médicos & Dentistas</Link>
+        <Link to="/" onClick={fecharMenu}>Médicos & Dentistas</Link>
       </div>
-      <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/voluntario'>Seja Voluntário</Link>
-        <img className={s.menuMobile} src={Menu} alt="Menu" />
+
+      <button
+        className={s.menuButton}
+        type="button"
+        aria-label={menuAberto ? 'Fechar menu' : 'Abrir menu'}
+        aria-expanded={menuAberto}
+        aria-controls="navegacao-principal"
+        onClick={() => setMenuAberto((aberto) => !aberto)}
+      >
+        <img src={Menu} alt="" aria-hidden="true" />
+      </button>
+
+      <nav
+        id="navegacao-principal"
+        className={menuAberto ? s.navAberta : ''}
+        aria-label="Navegação principal"
+      >
+        <Link to="/" onClick={fecharMenu}>Home</Link>
+        <Link to="/como-ajudar" onClick={fecharMenu}>Como ajudar</Link>
+        <Link to="/voluntario" onClick={fecharMenu}>Seja voluntário</Link>
       </nav>
     </header>
   )
